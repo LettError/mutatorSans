34c3222b9047c38b097708edc2e9b64a25f47b54
justvanrossum <justvanrossum@gmail.com>
2019-10-22 14:58:50 +0200

    build all four working .designspace files

---
+++ build_all_fontmake.sh
@@ -0,0 +1,4 @@
+fontmake -m MutatorSans.designspace -o variable
+fontmake -m MutatorSans-weight-only.designspace -o variable
+fontmake -m MutatorSans-width-only.designspace -o variable
+fontmake -m MutatorSans-with-openNodes.designspace -o variable
