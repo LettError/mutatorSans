c3dd53b4ff9ee6489fbab64904ce6e9f51e53e67
justvanrossum <justvanrossum@gmail.com>
2019-10-22 15:06:08 +0200

    make sure to abort on error

--- build_all_fontmake.sh
+++ build_all_fontmake.sh
@@ -1,3 +1,5 @@
+set -e  # make sure to abort on error
+
 fontmake -m MutatorSans.designspace -o variable
 fontmake -m MutatorSans-weight-only.designspace -o variable
 fontmake -m MutatorSans-width-only.designspace -o variable
